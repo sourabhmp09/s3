ba3d4e679d8163625e857af3b10518402bf3421f
sourabh lodhi <saurabhlodhi739@gmail.com>
2023-09-11 17:13:31 +0530

    first commit

--- Frontend/src/component/Config/Config.js
+++ Frontend/src/component/Config/Config.js
@@ -1,2 +1,2 @@
-export const CLIENT_ID = process.env.CLIENT_ID || "ATWKyNzO4EOqyN1I7rswDv-AJDVdCuriegDa2I_jH3hb39hhY3DwNOYB8ZX293hNjFl7iv-l4r3b3ldf"
+export const CLIENT_ID = process.env.CLIENT_ID 
 export const APP_SECRET = process.env.APP_SECRET || ""
