3b508863e8d807fa7cd7f69d4748cb3b0222eda8
sourabh lodhi <saurabhlodhi739@gmail.com>
2023-09-11 18:17:22 +0530

    m

---
+++ Frontend/build/precache-manifest.0e3f5dcfd3f56e3d85091ab8c19f9bdf.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "d0285b529c6a666088f3",
+    "url": "/static/css/main.0ce57f13.chunk.css"
+  },
+  {
+    "revision": "d0285b529c6a666088f3",
+    "url": "/static/js/main.43aa5cba.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "bdea07cacda23c607b9f",
+    "url": "/static/css/2.6e79a4b3.chunk.css"
+  },
+  {
+    "revision": "bdea07cacda23c607b9f",
+    "url": "/static/js/2.7d6531c6.chunk.js"
+  },
+  {
+    "revision": "3101c01df449722d9d093865f9b442ef",
+    "url": "/static/media/playstore.3101c01d.png"
+  },
+  {
+    "revision": "d0891202e1665975b793eabf8ac28aaa",
+    "url": "/static/media/Appstore.d0891202.png"
+  },
+  {
+    "revision": "782adc2b6062ab00461359da5b02b753",
+    "url": "/static/media/Profile.782adc2b.png"
+  },
+  {
+    "revision": "728216be61204d8fa61b63c9d9d0e1c1",
+    "url": "/static/media/cover.728216be.jfif"
+  },
+  {
+    "revision": "0916299bf46b2c86b003174c325de234",
+    "url": "/index.html"
+  }
+];
